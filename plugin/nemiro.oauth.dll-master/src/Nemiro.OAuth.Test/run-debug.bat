257f1f4d3930a0d6a978b067cb915b84380ee39f
BungPeerapat <mashiro74122541@gmail.com>
2020-04-09 20:15:25 +0700

    Save

--- plugin/nemiro.oauth.dll-master/src/Nemiro.OAuth.Test/run-debug.bat
+++
@@ -1,7 +0,0 @@
-chcp 65001
-:: --verbosity normal
-:: --configuration "Release"
-:: --logger "trx;LogFileName=results.trx"
-:: > output.log
-dotnet test > output.log
-notepad output.log
--- plugin/nemiro.oauth.dll-master/src/Nemiro.OAuth.Test/run-release.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-dotnet test --configuration "Release" > output.log
-notepad output.log
