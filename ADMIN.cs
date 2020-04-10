1af2768d2eeb199f6a0e6815ec98edb172712bab
BungPeerapat <mashiro74122541@gmail.com>
2020-04-10 16:15:25 +0700

    Save

--- ADMIN.cs
+++ ADMIN.cs
@@ -26,11 +26,11 @@ namespace ChatApplicationLearningSocket
         async private void bunifuImageButton1_Click(object sender, EventArgs e)
         {
             MenuOpen();
+            USERNAME.Visible = true;
+            await Task.Delay(500);
             chatzonebutton.Visible = true;
             await Task.Delay(500);
             bunifuFlatButton1.Visible = true;
-            await Task.Delay(500);
-            USERNAME.Visible = true;
             await Task.Delay(500);
             bunifuFlatButton2.Visible = true;
             await Task.Delay(500);
