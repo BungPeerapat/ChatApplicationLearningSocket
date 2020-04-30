f608871aae00c057d8b0e2532de91e909b652ff6
BungPeerapat <mashiro74122541@gmail.com>
2020-04-30 18:05:52 +0700

    Come back to Developer Mode

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -134,7 +134,6 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "ADMIN";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
@@ -148,7 +147,6 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "Member";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Login
